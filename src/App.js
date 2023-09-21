ceace6b8e28934e1b18b192e3601d2c055c1ffc8
Anum <anuum0058@gmail.com>
2023-09-21 14:09:33 +0500

    added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import './app.css'
+import './App.css'
+import Login from './pages/Login';
 const App = () => {
   return (
     <main className='App'>
-      <h1>Welcome to Task Trek</h1>
-      <p>A <span>Project Management System</span>, Where you can perform <span>CRUD </span>operations  on tasks.</p>
+      <Login />
     </main>
   )
 }
