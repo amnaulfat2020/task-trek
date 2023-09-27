90c5d29bb2a0f00d2d70ee9ade5d191733507f6f
abdulmoiz <moiza8684@gmail.com>
2023-09-27 17:40:35 +0500

    addded new firebase

--- src/utils/constants/Firebase.js
+++ src/utils/constants/Firebase.js
@@ -1,15 +1,12 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCR3Jp7UX7pATWQ0Z9ZajSi1BXiRSbBZtk",
-  authDomain: "task-trek-b3130.firebaseapp.com",
-  projectId: "task-trek-b3130",
-  storageBucket: "task-trek-b3130.appspot.com",
-  messagingSenderId: "1029260624599",
-  appId: "1:1029260624599:web:e22c21087ba2bfb3c82692",
-  measurementId: "G-NGCQBMQ3FY"
+  apiKey: "AIzaSyDAk4dY00qmAKeQ8Ndx8ySZEIiwguzIn7I",
+  authDomain: "task-trek-c6b75.firebaseapp.com",
+  projectId: "task-trek-c6b75",
+  storageBucket: "task-trek-c6b75.appspot.com",
+  messagingSenderId: "1033846365318",
+  appId: "1:1033846365318:web:ade2c5ae2e80050b204507"
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
