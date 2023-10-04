09ea24c2f396f42265c3f48eab6b5c76597f06ee
Anum <anuum0058@gmail.com>
2023-10-04 14:38:37 +0500

    added

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,39 +1,36 @@
-import React, { useState, useEffect } from "react";
-import { Card, Progress, List, Button, Modal, Input } from "antd";
-import { DeleteOutlined, PlusOutlined } from "@ant-design/icons";
-import {
-  createProject,
-  fetchProjects,
-  updateProject,
-  deleteProject,
-} from "../../services/api";
-import EditSvg from "../../assets/images/edit-pencil 1.svg";
-import redDotSvg from "../../assets/images/Ellipse red.svg";
-import greenDotSvg from "../../assets/images/Ellipse 12.svg";
-import yellowDotSvg from "../../assets/images/Ellipse yellow.svg";
-import Line3 from "../../assets/images/Line 3.png";
-import "./project.css";
-import AppHeader from "../../layout/MenuBar";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Card, Progress, List, Button, Modal, Input } from 'antd'; 
+import { DeleteOutlined, PlusOutlined } from '@ant-design/icons';
+import {createProject, fetchProjects, updateProject, deleteProject,} from '../../services/api';
+import EditSvg from '../../assets/images/edit-pencil 1.svg';
+import redDotSvg from '../../assets/images/Ellipse red.svg';
+import greenDotSvg from '../../assets/images/Ellipse 12.svg';
+import yellowDotSvg from '../../assets/images/Ellipse yellow.svg';
+import Line3 from '../../assets/images/Line 3.png';
+import './project.css';
+import AppHeader from '../../layout/MenuBar';
+import { Link } from 'react-router-dom';
+import { useSearch , useMenuContext } from '../../contexts/SearchContext';
+
 
 const Project = () => {
   const [projects, setProjects] = useState([]);
   const [newProject, setNewProject] = useState({
-    title: "",
-    start_Date: "",
-    status: "In Progress",
-    members: "",
-    progress: 0,
+    title: '',
+    start_Date: '',
+    status: 'In Progress', 
+    members: '',
+    progress: 0, 
   });
   // const [editingProject, setEditingProject] = useState(null);
 
   const [showInputFields, setShowInputFields] = useState(false);
   const [taskModalVisible, setTaskModalVisible] = useState(false);
   const [taskList, setTaskList] = useState([]);
-  const [taskText, setTaskText] = useState("");
-  const [editingProjectId, setEditingProjectId] = useState(null);
-  const [editingStartDate, setEditingStartDate] = useState(null);
-  const [editingMembers, setEditingMembers] = useState(null);
+  const [taskText, setTaskText] = useState('');
+  const [editingProjectId, setEditingProjectId] = useState(null); 
+  const [editingStartDate, setEditingStartDate] = useState(null); 
+  const [editingMembers, setEditingMembers] = useState(null); 
 
   useEffect(() => {
     async function fetchProjectData() {
@@ -52,9 +49,9 @@ const Project = () => {
   };
 
   const handleTaskAdd = () => {
-    if (taskText.trim() !== "") {
+    if (taskText.trim() !== '') {
       setTaskList([...taskList, taskText]);
-      setTaskText("");
+      setTaskText('');
     }
   };
 
@@ -64,14 +61,16 @@ const Project = () => {
     setTaskList(updatedTaskList);
   };
 
-  const handleSubmit = async (e) => {
+
+
+ const handleSubmit = async (e) => {
     e.preventDefault();
     await createProject({ ...newProject, tasks: taskList });
     setNewProject({
-      title: "",
-      start_Date: "",
-      status: "In Progress",
-      members: "",
+      title: '',
+      start_Date: '',
+      status: 'In Progress',
+      members: '',
       progress: 0,
     });
     setTaskList([]);
@@ -93,22 +92,16 @@ const Project = () => {
 
   const handleUpdate = async () => {
     if (editingProjectId !== null) {
-      const currentProject = projects.find(
-        (project) => project.id === editingProjectId
-      );
+      const currentProject = projects.find((project) => project.id === editingProjectId);
       const updatedProject = {
         ...currentProject,
-        Start_Date:
-          editingStartDate !== null
-            ? editingStartDate
-            : currentProject.StartDate,
-        members:
-          editingMembers !== null ? editingMembers : currentProject.members,
+        Start_Date: editingStartDate !== null ? editingStartDate : currentProject.StartDate,
+        members: editingMembers !== null ? editingMembers : currentProject.members,
         status: newProject.status,
         progress: newProject.progress,
       };
       await updateProject(editingProjectId, updatedProject);
-      setEditingProjectId(null);
+      setEditingProjectId(null); 
       const updatedProjectList = await fetchProjects();
       setProjects(updatedProjectList);
     }
@@ -127,28 +120,38 @@ const Project = () => {
     setProjects(updatedProjects);
   };
 
+  
+  const { searchQuery } = useSearch(); // Access the searchQuery from the context
+  const { menuFilter } = useMenuContext();
+
+
   const cardRender = (project) => {
-    const { title, start_Date, status, members, progress } = project
-     
+    const { title, start_Date, status, members, progress } = project;
 
-    let statusImg = redDotSvg;
-    let statusColor = "red";
-    if (status === "Completed") {
-      statusImg = greenDotSvg;
-      statusColor = "green";
-    } else if (status === "On Hold" || status === "Review") {
-      statusImg = yellowDotSvg;
-      statusColor = "yellow";
-    }
+let statusImg= redDotSvg;
+let statusColor = 'red';
+if(status === 'Completed'){
+  statusImg= greenDotSvg;
+  statusColor= 'green';
+}
+else if(status === 'On Hold' || status === 'Review'){
+  statusImg= yellowDotSvg;
+  statusColor = 'yellow';
+}
 
-    let color = "red";
+
+    let color = 'red'; 
     if (progress >= 50) {
-      color = "light";
+      color = 'light';
     }
     if (progress === 100) {
-      color = "green";
+      color = 'green';
     }
 
+
+    const filteredBySearch = !searchQuery || project.title.toLowerCase().includes(searchQuery.toLowerCase());
+    const filteredByMenu = menuFilter ==='All' ||  project.status === menuFilter;
+    if (filteredBySearch && filteredByMenu)
     return (
       <div className="card-render" key={project.id}>
         <Card>
@@ -156,35 +159,28 @@ const Project = () => {
           <div className="card-header">
             <h1>{title}</h1>
             <div className="icon">
-              {editingProjectId === project.id ? (
-                <button type="primary" onClick={handleUpdate}>
-                  Update
-                </button>
-              ) : (
-                <div>
-                  <button onClick={() => handleEdit(project.id)}>
-                    <span>
-                      {" "}
-                      <img src={EditSvg} alt="edit icon" />{" "}
-                    </span>
-                  </button>
-                  <button onClick={() => handleDelete(project.id)}>
-                    <span>
-                      {" "}
-                      <DeleteOutlined />{" "}
-                    </span>
-                  </button>
-                </div>
-              )}
+            {editingProjectId === project.id ? (
+            <button type="primary" onClick={handleUpdate}>
+              Update
+            </button>
+           ) : (
+            <div>
+            <button onClick={() => handleEdit(project.id)}>
+            <span> <img src={EditSvg} alt="edit icon"  /> </span>
+            </button>
+            <button onClick={() => handleDelete(project.id)}>
+              <span> <DeleteOutlined /> </span>
+            </button>
+            </div>
+           )}
+
+           
             </div>
           </div>
-          {/* status */}
+                {/* status */}
           <div className="status">
-            <span>
-              {" "}
-              <img src={statusImg} alt="dot" />{" "}
-            </span>
-            {editingProjectId === project.id ? (
+             <span> <img src={statusImg} alt='dot'/> </span>
+             {editingProjectId === project.id ? (
               <select
                 name="status"
                 value={newProject.status}
@@ -193,95 +189,93 @@ const Project = () => {
                 }}
               >
                 {/* Options for status */}
-                <option value="In Progress">In Progress</option>
+                <option value="In Progress" >In Progress</option>
                 <option value="Discussing">Discussing</option>
-                <option value="Completed">Completed</option>
+                <option  value="Completed">Completed</option>
                 <option value="Review">Review</option>
                 <option value="Cancelled">Cancelled</option>
-                <option value="On Hold">On Hold</option>
+                <option  value="On Hold">On Hold</option>
               </select>
             ) : (
-              <p className={statusColor}>{status}</p>
+              <p className={statusColor}  >{status}</p>
             )}
           </div>
           <div className="Task-area">
-            {/* start date */}
-            <div className="startDate">
-              <p>Start Date</p>
-              {editingProjectId === project.id ? (
-                <input
-                  type="date"
-                  name="start_Date"
-                  value={editingStartDate !== null ? editingStartDate : ""}
-                  onChange={(e) => setEditingStartDate(e.target.value)}
-                />
-              ) : (
-                <p>
-                  {localStorage.getItem(`startDate_${project.id}`) ||
-                    project.StartDate}
-                </p>
-              )}
-            </div>
-            <div className="tasks-box">
-              <div className="tasks">
-                <p style={{ marginLeft: "8px" }}>{14}</p>
-                {project.tasks && project.tasks.length > 0 && (
-                  <div className="task-list">
-                    <h3>Tasks:</h3>
-                    <List
-                      dataSource={project.tasks}
-                      renderItem={(task, index) => (
-                        <List.Item key={index}>
-                          {task}
-                          <Button
-                            type="text"
-                            onClick={() => handleTaskDelete(index)}
-                          >
-                            Delete
-                          </Button>
-                        </List.Item>
-                      )}
-                    />
-                  </div>
+          {/* start date */}
+          <div className="startDate">
+            <p>Start Date</p>
+            {editingProjectId === project.id ? (
+            <input
+              type="date"
+              name="start_Date"
+              value={editingStartDate !== null ? editingStartDate : ''}
+              onChange={(e) => setEditingStartDate(e.target.value)}
+            />
+            ) : (
+              <p>{localStorage.getItem(`startDate_${project.id}`) || project.StartDate}</p>
+            )}
+          </div>
+              <div className="tasks-box">
+                <div className="tasks">
+                  <p style={{ marginLeft: "8px"}}>{ 14 }</p>
+                  {project.tasks && project.tasks.length > 0 && (
+            <div className="task-list">
+              <List
+                dataSource={project.tasks}
+                renderItem={(task, index) => (
+                  <List.Item key={index}>
+                    {task}
+                    <Button type="text" onClick={() => handleTaskDelete(index)}>
+                      Delete
+                    </Button>
+                  </List.Item>
                 )}
-                <div className="task-input">
-                  <Link to="/tasks">
-                    <p>Tasks</p>
-                  </Link>
-                  <Modal
-                    title="Add Task"
-                    open={taskModalVisible}
-                    onOk={handleTaskAdd}
-                    onCancel={toggleTaskModal}
-                  >
-                    <Input
-                      placeholder="Task Name"
-                      value={taskText}
-                      onChange={handleTaskInputChange}
-                    />
-                  </Modal>
+              />
+            </div>
+          )}
+          <div className="task-input">
+            <Link to="/tasks">
+               <p>Tasks</p>
+            </Link>
+            <Modal
+              title="Add Task"
+              open={taskModalVisible}
+              onOk={handleTaskAdd}
+              onCancel={toggleTaskModal}
+            >
+              <Input
+                placeholder="Task Name"
+                value={taskText}
+                onChange={handleTaskInputChange}
+              />
+            </Modal>
+          </div>
+                
                 </div>
-              </div>
               <div className="line3">
-                <img src={Line3} alt="line3" />
+                  <img src={Line3} alt="line3" />
               </div>
 
-              <div className="users">
-                <p style={{ marginLeft: "12px" }}>{4}</p>
-                <p>Users</p>
+                <div className="users">
+                  <p style={{ marginLeft: "12px"}}>{4}</p>
+                  <p>Users</p>
+                </div>
+
               </div>
-            </div>
+
+
+
           </div>
-          {/* members */}
+         {/* members */}
           <div className="attribute">
             <p>Members</p>
             {editingProjectId === project.id ? (
-              <input
-                type="text"
-                name="members"
-                value={editingMembers !== null ? editingMembers : members}
-                onChange={(e) => setEditingMembers(e.target.value)}
-              />
+            <input
+              type="text"
+              name="members"
+              value={editingMembers !== null ? editingMembers : members}
+              onChange={(e) => setEditingMembers(e.target.value)}
+            />
             ) : (
               <p>{members}</p>
             )}
@@ -307,6 +301,7 @@ const Project = () => {
               </div>
             )}
           </div>
+          
         </Card>
       </div>
     );
@@ -314,14 +309,10 @@ const Project = () => {
 
   return (
     <div>
-      <AppHeader />
+      <AppHeader/>
       <div>
-        <h1>Projects</h1>
-        <Button
-          type="primary"
-          icon={<PlusOutlined />}
-          onClick={toggleInputFields}
-        >
+      <h1>Projects</h1>
+        <Button type="primary" icon={<PlusOutlined />} onClick={toggleInputFields}>
           New Project
         </Button>
       </div>
@@ -341,9 +332,14 @@ const Project = () => {
           </Button>
         </form>
       )}
+      
 
       <div className="card">
-        {projects.map((project) => cardRender(project))}
+        {projects.map((project) => (
+          <div key={project.id}>
+            {cardRender(project)}
+          </div>
+        ))}
       </div>
     </div>
   );
