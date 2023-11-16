13a2fafeca3775c22540cd5367f3babda626d6ea
abdulmoiz <moiza8684@gmail.com>
2023-11-16 16:56:37 +0500

    done the theme

--- src/layout/MenuBar.js
+++ src/layout/MenuBar.js
@@ -6,10 +6,10 @@ import { doc, getDoc } from 'firebase/firestore';
 import { Link, useNavigate } from 'react-router-dom';
 import { Input, Space, Badge, Avatar, Typography, Menu, Dropdown, Modal } from 'antd';
 import {
-SearchOutlined,
-BellOutlined,
-UserOutlined,
-LogoutOutlined,
+    SearchOutlined,
+    BellOutlined,
+    UserOutlined,
+    LogoutOutlined,
 } from '@ant-design/icons';
 import { useSearch } from '../contexts/SearchContext';
 import headerStyles from '../styles/headerStyles';
@@ -17,110 +17,112 @@ const Title = Typography;
 
 
 const MenuBar = ({ currentPage }) => {
-const { searchQuery, setSearch } = useSearch();
-const [showProfilePopup, setShowProfilePopup] = useState(false);
-const [userData, setUserData] = useState(null);
-const [showWelcomeModal, setShowWelcomeModal] = useState(false);
-const [loggedIn, setLoggedIn] = useState(false);
-const navigate = useNavigate();
-useEffect(() => {
-const user = auth.currentUser;
-if (user) {
-setLoggedIn(true);
-showWelcomeNotification();
-}
-}, []);
-useEffect(() => {
-async function fetchUserData() {
-try {
-const user = auth.currentUser;
-if (user) {
-const userRef = doc(db, 'users', user.email);
-const userDoc = await getDoc(userRef);
-if (userDoc.exists()) {
-const userData = userDoc.data();
-setUserData({ ...userData, email: user.email });
-}
-}
-} catch (error) {
-console.error('Error fetching user data:', error);
-}
-}
+    const { searchQuery, setSearch } = useSearch();
+    const [showProfilePopup, setShowProfilePopup] = useState(false);
+    const [userData, setUserData] = useState(null);
+    const [showWelcomeModal, setShowWelcomeModal] = useState(false);
+    const [loggedIn, setLoggedIn] = useState(false);
+    const navigate = useNavigate();
+    useEffect(() => {
+        const user = auth.currentUser;
+        if (user) {
+            setLoggedIn(true);
+            showWelcomeNotification();
+        }
+    }, []);
+    useEffect(() => {
+        async function fetchUserData() {
+            try {
+                const user = auth.currentUser;
+                if (user) {
+                    const userRef = doc(db, 'users', user.email);
+                    const userDoc = await getDoc(userRef);
+                    if (userDoc.exists()) {
+                        const userData = userDoc.data();
+                        setUserData({ ...userData, email: user.email });
+                    }
+                }
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        }
 
 
-fetchUserData();
-}, []);
+        fetchUserData();
+    }, []);
 
 
-const handleClick = () => {
-signOut(auth)
-.then(() => {
-navigate('/');
-console.log('User signed out successfully');
-})
-.catch((err) => {
-console.log(err);
-});
-};
+    const handleClick = () => {
+        signOut(auth)
+            .then(() => {
+                navigate('/');
+                console.log('User signed out successfully');
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
 
 
-const menu = (
-<Menu>
-<Menu.Item key="profile" icon={<UserOutlined />} onClick={() => setShowProfilePopup(true)}>
-Profile
-</Menu.Item>
-<Menu.Item key="logout" icon={<LogoutOutlined />} onClick={handleClick}>
-Logout
-</Menu.Item>
-</Menu>
-);
+    const menu = (
+        <Menu>
+            <Menu.Item key="profile" icon={<UserOutlined />} onClick={() => setShowProfilePopup(true)}>
+                Profile
+            </Menu.Item>
+            <Menu.Item key="logout" icon={<LogoutOutlined />} onClick={handleClick}>
+                Logout
+            </Menu.Item>
+        </Menu>
+    );
 
 
 
-const showWelcomeNotification = () => {
-setShowWelcomeModal(true);
-};
-return (
-<div style={headerStyles.container}>
-<div style={headerStyles.leftSection}>
-{currentPage}
-</div>
-<div style={headerStyles.centerSection}>
-<Space>
-<Input
-prefix={<SearchOutlined />}
-placeholder="Search..."
-style={{ ...headerStyles.searchInput, width: '700px',height:'40px' }}
-value={searchQuery}
-// onChange={handleSearchChange}
-/>
-</Space>
-</div>
-<div style={headerStyles.rightSection}>
-<Space size="large">
-<Badge dot>
-<BellOutlined style={headerStyles.icon} onClick={showWelcomeNotification} />
-</Badge>
-<Dropdown overlay={menu} trigger={['click']}>
-<Avatar icon={<UserOutlined />} style={headerStyles.avatar} onClick={() => setLoggedIn(!loggedIn)} />
-</Dropdown>
-</Space>
-</div>
-{showProfilePopup && userData && (
-<UserProfilePopup userData={userData} onClose={() => setShowProfilePopup(false)} />
-)}
-{loggedIn && (
-<Modal
-title="Welcome to Task Trek!"
-visible={showWelcomeModal}
-onOk={() => setShowWelcomeModal(false)}
-onCancel={() => setShowWelcomeModal(false)}
->
-<p>You have successfully logged in. Enjoy your time with Task Trek!</p>
-</Modal>
-)}
-</div>
-);
+    const showWelcomeNotification = () => {
+        setShowWelcomeModal(true);
+    };
+    return (
+        <div style={headerStyles.container}>
+            <div style={headerStyles.leftSection} >
+                <Title style={headerStyles.pageTitle}>
+                    {currentPage}
+                </Title>
+            </div>
+            <div style={headerStyles.centerSection}>
+                <Space>
+                    <Input
+                        prefix={<SearchOutlined />}
+                        placeholder="Search..."
+                        style={{ ...headerStyles.searchInput, width: '700px', height: '40px' }}
+                        value={searchQuery}
+                    // onChange={handleSearchChange}
+                    />
+                </Space>
+            </div>
+            <div style={headerStyles.rightSection}>
+                <Space size="large">
+                    <Badge dot>
+                        <BellOutlined style={headerStyles.icon} onClick={showWelcomeNotification} />
+                    </Badge>
+                    <Dropdown overlay={menu} trigger={['click']}>
+                        <Avatar icon={<UserOutlined />} style={headerStyles.avatar} onClick={() => setLoggedIn(!loggedIn)} />
+                    </Dropdown>
+                </Space>
+            </div>
+            {showProfilePopup && userData && (
+                <UserProfilePopup userData={userData} onClose={() => setShowProfilePopup(false)} />
+            )}
+            {loggedIn && (
+                <Modal
+                    title="Welcome to Task Trek!"
+                    visible={showWelcomeModal}
+                    onOk={() => setShowWelcomeModal(false)}
+                    onCancel={() => setShowWelcomeModal(false)}
+                >
+                    <p>You have successfully logged in. Enjoy your time with Task Trek!</p>
+                </Modal>
+            )}
+        </div>
+    );
 };
 
 
