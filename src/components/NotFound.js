36e4f257b9f579785e5b73df49c88f1e29d82c38
amnaulfat2020 <127995276+amnaulfat2020@users.noreply.github.com>
2023-11-27 16:41:55 +0500

    changes

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { Result, Button } from 'antd';
 import { Link } from 'react-router-dom';
@@ -6,14 +5,17 @@ import { Link } from 'react-router-dom';
 const NotFound = () => {
   return (
     <Result
-      status="404"
-      title="404"
-      subTitle="Sorry, the page you visited does not exist."
-      extra={
-        <Link to="/">
-          <Button type="primary">Back Home</Button>
+    status="404"
+    title="404"
+    subTitle={<span style={{ fontFamily: '"Montserrat", sans-serif' }}>Sorry, the page you visited does not exist.</span>}
+    extra={
+      <Link to="/">
+          <Button type="primary" style={{ backgroundColor: 'rgb(22, 119, 255)', borderRadius: 0, fontFamily: 'Montserrat' }}>
+            Back Home
+          </Button>
         </Link>
       }
+      style={{ fontFamily: '"Montserrat", sans-serif', marginTop: '20px' }}
     />
   );
 };
